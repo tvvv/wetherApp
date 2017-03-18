d58d0143a19f19a64990ccd6495956e67aabd986
Volodymyr Dey <volodymyr.dey@eleks.com>
2017-03-18 12:11:41 +0200

    Add xml 1 part

--- js/xml_request.js
+++ js/xml_request.js
@@ -5,10 +5,10 @@ function search() {
   var city_name = document.querySelector("div.search input[name=city]").value;
   if (city_name === "") {
     document.getElementById("result_city").innerHTML = "Please enter city name";
-    set_style("#result_city {color: red;}");
+    set_style("#result_city {color: red; margin-top: 38px; } input{ margin-top: 75px;} .search_button { margin-top: 77px;}");
   } else {
     document.getElementById("result_city").innerHTML = "Weather in city " + city_name;
-    set_style("#result_city {color: white;}");
+    set_style("#result_city {color: white;margin-top: 5px; transition: 1s;} input{ margin-top: 5px;} .search_button { margin-top: 7px;}");
   }
 };
 
@@ -18,3 +18,11 @@ function set_style(style) {
   x.appendChild(t);
   document.head.appendChild(x);
 };
+
+// function request() {
+//   var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q="+ city_name +"&APPID=cb64092b9a1a76433cad8b70805f1d41";
+//   var xml = new XMLHttpRequest();
+//   xml.open("GET", apiUrl, false);
+//   xml.send();
+//
+// }
