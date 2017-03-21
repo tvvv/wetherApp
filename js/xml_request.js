c4516acb044a7f2e56a29735b58d4eb481bb287d
Volodymyr Dey <volodymyr.dey@eleks.com>
2017-03-21 17:21:24 +0200

    Add fuctionality to show all data in table

--- js/xml_request.js
+++ js/xml_request.js
@@ -6,6 +6,8 @@ function search() {
   if (city_name === "") {
     document.getElementById("result_city").innerHTML = "Please enter city name";
     set_style("#result_city {color: red; margin-top: 38px; transition: 1s;} input{ margin-top: 75px; transition: 1s;} .search_button { margin-top: 77px; transition: 1s;}");
+    var tbl = document.getElementById("resultTable");
+    if(tbl) tbl.parentNode.removeChild(tbl);
   } else {
     document.getElementById("result_city").innerHTML = "Weather in city " + city_name;
     set_style("#result_city {color: white;margin-top: 5px; transition: 1s;} input{ margin-top: 5px;} .search_button { margin-top: 7px;}");
@@ -31,51 +33,49 @@ function request() {
   } else {
     var weather_obj = JSON.parse(xml.responseText);
 
-    var body = document.getElementsByTagName("body")[0];
-    var result_table = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-    result_table.setAttribute("id", "tableStyle");
-    var jsonElement = 0;
-    for (var i = 0; i < 17; i++) {
-      var row = document.createElement("tr");
-
-      for (var j = 0; j < 6; j++) {
-        var cell = document.createElement("td");
-        var cell_text;
-        if (i === 0) {
-          cell.setAttribute("id", "heder");
-        } else {
-          cell.setAttribute("id", "cellStyle");
-        }
-        if ( i === 0) {
-          cell_text = document.createTextNode(weather_obj.list[jsonElement].dt_txt.substring(0, weather_obj.list[jsonElement].dt_txt.length - 9)) ;
-        } else if ( i !== 0 && i % 2 === 1) {
-          cell_text = document.createTextNode(weather_obj.list[jsonElement].dt_txt.substring(11, weather_obj.list[jsonElement].dt_txt.length) + "\r\n" +
-                      "Temperature " + Math.floor(weather_obj.list[jsonElement].main.temp_min - 270) + " °C" + "\r\n" +
-                      weather_obj.list[jsonElement].weather[0].description);
-        } else {
-          cell_text = document.createElement("img");
-          cell_text.setAttribute("src", "http://openweathermap.org/img/w/" + weather_obj.list[i].weather[0].icon + ".png");
-        }
-        cell.appendChild(cell_text);
-        row.appendChild(cell);
-        jsonElement++;
-      }
-      tblBody.appendChild(row);
-    }
-    result_table.appendChild(tblBody);
-    body.appendChild(result_table);
-
-
-    // document.getElementById("1 DayDate").innerHTML = weather_obj.list[0].dt_txt.substring(0, weather_obj.list[0].dt_txt.length - 9) ;
-    // document.getElementById("1 record").innerHTML = weather_obj.list[0].dt_txt.substring(11, weather_obj.list[0].dt_txt.length) + "\r\n" +
-    //                                                   "Temperature " + Math.floor(weather_obj.list[0].main.temp_min - 270) + " °C" + "\r\n" +
-    //                                                   weather_obj.list[0].weather[0].description;
-    // var img_link = "http://openweathermap.org/img/w/" + weather_obj.list[0].weather[0].icon + ".png";
-    // document.getElementById("1 image").src = "http://openweathermap.org/img/w/" + weather_obj.list[0].weather[0].icon + ".png";
-    //
-    // if (weather_obj.list[0].dt_txt.substring(0, weather_obj.list[0].dt_txt.length - 9) === weather_obj.list[1].dt_txt.substring(0, weather_obj.list[1].dt_txt.length - 9)) {
-    //
-    // }
+  var result_array = [[],[],[],[]];
+  for (var i = 0; i < weather_obj.list.length; i++) {
+    result_array[0][i] = weather_obj.list[i].dt_txt.substring(0, weather_obj.list[i].dt_txt.length - 9) + "  " + weather_obj.list[i].dt_txt.substring(11, weather_obj.list[i].dt_txt.length);
+    result_array[1][i] = "Temperature " + Math.floor(weather_obj.list[i].main.temp_min - 270) + " °C";
+    result_array[2][i] = weather_obj.list[i].weather[0].description;
+    result_array[3][i] = "http://openweathermap.org/img/w/" + weather_obj.list[i].weather[0].icon + ".png";
   }
+
+  createTable(result_array);
+
+  }
+}
+
+function createTable(tableData) {
+  var row_count = 0;
+  var table = document.createElement('table');
+  table.setAttribute("id", "resultTable");
+  var tableBody = document.createElement('tbody');
+
+  tableData.forEach(function(rowData) {
+    var row = document.createElement('tr');
+
+    rowData.forEach(function(cellData) {
+      var cell = document.createElement('td');
+      cell.setAttribute("id", "cellStyle");
+      switch (row_count) {
+        case 3:
+          var cellImgData = document.createElement("img");
+          cellImgData.setAttribute("src", cellData);
+          cell.appendChild(cellImgData);
+          break;
+        default:
+          cell.appendChild(document.createTextNode(cellData));
+
+      }
+
+      row.appendChild(cell);
+    });
+
+    row_count++;
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
 }
